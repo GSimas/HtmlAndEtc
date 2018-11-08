a43f076eccd0eeec0a60fd653c337c9772fcc259
Gustavo Simas da Silva <gustavosimassilva@gmail.com>
2018-11-08 08:41:52 -0200

    Html5up Strata standard theme
    standard theme for Strata instead of MHC website

--- Templates/html5up-strata/assets/js/main.js
+++ Templates/html5up-strata/assets/js/main.js
@@ -98,7 +98,7 @@
 		// Lightbox gallery.
 			$window.on('load', function() {
 
-				$('#ten').poptrox({
+				$('#two').poptrox({
 					caption: function($a) { return $a.next('h3').text(); },
 					overlayColor: '#2c2c2c',
 					overlayOpacity: 0.85,
